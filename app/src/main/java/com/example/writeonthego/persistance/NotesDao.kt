2593fdbbfbe961d743881d91c913f9901d224382
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-02-18 15:54:07 +0530

    added get note by id in NotesDao class

--- app/src/main/java/com/example/writeonthego/persistance/NotesDao.kt
+++ app/src/main/java/com/example/writeonthego/persistance/NotesDao.kt
@@ -1,30 +1,19 @@
 package com.example.writeonthego.persistance
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.example.writeonthego.model.Note
 
-@Dao
-interface NotesDao {
+@Dao interface NotesDao {
 
+    @Query("SELECT * FROM Notes WHERE notes.id=:id") fun getNoteById(id: Int) : Note?
 
-    //TODO getnotesByID
+    @Query("SELECT * FROM Notes ORDER BY dateUpdated DESC") fun getNotes() : LiveData<List<Note>>
 
+    @Delete fun deleteNote(note: Note) : Int
 
-    @Query("SELECT * FROM Notes ORDER BY dateUpdated DESC")
-    fun getNotes(): LiveData<List<Note>>
+    @Update fun updateNote(note: Note) : Int
 
-    @Delete
-    fun deleteNote(note: Note): Int
-
-    @Update
-    fun updateNote(note: Note): Int
-
-    @Insert
-    fun insertNote(note: Note)
+    @Insert fun insertNote(note: Note)
 
 }
