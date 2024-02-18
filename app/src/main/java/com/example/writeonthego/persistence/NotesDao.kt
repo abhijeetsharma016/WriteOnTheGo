2c9650bd06d68234d5f7b2d53326db3fd9037ed3
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-02-18 10:31:25 +0530

    Created NotesDao

---
+++ app/src/main/java/com/example/writeonthego/persistence/NotesDao.kt
@@ -0,0 +1,28 @@
+package com.example.writeonthego.persistence
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.writeonthego.model.Note
+
+
+@Dao
+interface NotesDao {
+    @Query("SELECT * FROM Notes WHERE notes.id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Query("SELECT * FROM Notes ORDER BY dateUpdated DESC")
+    fun getNotes():LiveData<List<Note>>
+
+    @Delete
+    fun deleteNote(note: Note): Int
+
+    @Update
+    fun updateNote(note: Note): Int
+
+    @Insert
+    fun insertNote(note: Note)
+}
---
+++ app/src/main/java/com/example/writeonthego/persistence/NotesDatabase.kt
@@ -0,0 +1,5 @@
+package com.example.writeonthego.persistence
+
+class NotesDatabase {
+
+}
