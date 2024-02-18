6c96238b7298fe10e7eff4400ee272c6a86dac44
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-02-18 10:58:04 +0530

    created NotesDatabase

--- app/src/main/java/com/example/writeonthego/persistence/NotesDatabase.kt
+++ app/src/main/java/com/example/writeonthego/persistence/NotesDatabase.kt
@@ -1,5 +1,13 @@
 package com.example.writeonthego.persistence
 
-class NotesDatabase {
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.room.Database
+import com.example.writeonthego.model.Note
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Database(entities = [Note::class], version = 1)
+abstract class NotesDatabase {
+abstract fun NotesDao(): NotesDao
 
 }
